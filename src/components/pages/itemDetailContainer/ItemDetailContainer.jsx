cec10b75912cba06334f850b541c5b51322c2ef6
irinag00 <irinagorlino@gmail.com>
2023-10-16 20:54:28 -0300

    cambie de lugar el let prodcto

--- src/components/pages/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/pages/itemDetailContainer/ItemDetailContainer.jsx
@@ -3,10 +3,11 @@ import { products } from "../../../productsMock";
 import ItemDetail from "./ItemDetail";
 import { useParams } from "react-router-dom";
 const ItemDetailContainer = () => {
-  const [productSelected, setProductSelected] = useState([]);
+  const [productSelected, setProductSelected] = useState({});
   const { id } = useParams();
-  let producto = products.find((product) => product.id === +id);
+
   useEffect(() => {
+    let producto = products.find((product) => product.id === +id);
     const getProducts = new Promise((resolve, reject) => {
       resolve(producto);
     });
