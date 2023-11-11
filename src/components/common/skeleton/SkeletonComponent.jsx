bd9b5e487fa37df9402bfc34a2a9496463d85daa
irinag00 <irinagorlino@gmail.com>
2023-11-11 09:37:54 -0300

    descargue sweetalert y condicione el carrito

--- src/components/common/skeleton/SkeletonComponent.jsx
+++ src/components/common/skeleton/SkeletonComponent.jsx
@@ -7,44 +7,22 @@ import {
 const SkeletonComponent = () => {
   return (
     <div className="container grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 group transition animate-pulse">
-      <Card className="w-90 my-3">
-        <CardHeader shadow={false} floated={false} className="h-90">
-          <div className="w-full h-60 bg-gray-300 rounded max-sm:w-96"></div>
-        </CardHeader>
-        <CardBody className="flex mx-auto">
-          <div className="items-center justify-center">
-            <div className="w-full h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-2 mt-3 items-center sm:w-72"></div>
-            <div className="w-full h-3 bg-gray-200 rounded-md dark:bg-gray-700 mb-2 mt-3 sm:w-36"></div>
-            <div className="w-full h-20 bg-gray-200 rounded-md dark:bg-gray-700  mb-2 mt-3 sm:w-72"></div>
-          </div>
-        </CardBody>
-      </Card>
-      <Card className="w-90 my-3">
-        <CardHeader shadow={false} floated={false} className="h-90">
-          <div className="w-full h-60 bg-gray-300 rounded max-sm:w-96"></div>
-        </CardHeader>
-        <CardBody className="flex mx-auto">
-          <div className="items-center justify-center">
-            <div className="w-full h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-2 mt-3 items-center sm:w-72"></div>
-            <div className="w-full h-3 bg-gray-200 rounded-md dark:bg-gray-700 mb-2 mt-3 sm:w-36"></div>
-            <div className="w-full h-20 bg-gray-200 rounded-md dark:bg-gray-700  mb-2 mt-3 sm:w-72"></div>
-          </div>
-        </CardBody>
-      </Card>
-      <Card className=" w-90 my-3">
-        <CardHeader shadow={false} floated={false} className="h-90">
-          <div className="w-full h-60 bg-gray-300 rounded max-sm:w-96"></div>
-        </CardHeader>
-        <CardBody className="flex mx-auto">
-          <div className="items-center justify-center">
-            <div className="w-full h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-2 mt-3 items-center sm:w-72"></div>
-            <div className="w-full h-3 bg-gray-200 rounded-md dark:bg-gray-700 mb-2 mt-3 sm:w-36"></div>
-            <div className="w-full h-20 bg-gray-200 rounded-md dark:bg-gray-700  mb-2 mt-3 sm:w-72"></div>
-          </div>
-        </CardBody>
-      </Card>
+      {[...Array(6)].map((_, index) => (
+        <Card key={index} className="w-90 my-3">
+          <CardHeader shadow={false} floated={false} className="h-90">
+            <div className="w-full h-60 bg-gray-300 rounded max-sm:w-96"></div>
+          </CardHeader>
+          <CardBody className="flex mx-auto">
+            <div className="items-center justify-center">
+              <div className="w-full h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-2 mt-3 items-center sm:w-72"></div>
+              <div className="w-full h-3 bg-gray-200 rounded-md dark:bg-gray-700 mb-2 mt-3 sm:w-36"></div>
+              <div className="w-full h-20 bg-gray-200 rounded-md dark:bg-gray-700  mb-2 mt-3 sm:w-72"></div>
+            </div>
+          </CardBody>
+        </Card>
+      ))}
+      ;
     </div>
   );
 };
-
 export default SkeletonComponent;
--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -2,26 +2,30 @@ import { Link } from "react-router-dom";
 import { Button, Typography, Tooltip } from "@material-tailwind/react";
 import { BsFillTrashFill, BsCheckLg } from "react-icons/bs";
 import { RiErrorWarningLine } from "react-icons/ri";
+import { BiSolidShoppingBags } from "react-icons/bi";
 
 const Cart = ({ cart, clearCart, deleteProductById, total }) => {
   return (
     <div className="container">
-      <div className="flex flex-row justify-between text-center">
-        <Typography variant="h3" className="text-pinkLogo text-center my-3">
-          Productos
-        </Typography>
-        <Tooltip content="Eliminar todos los productos del carrito">
-          <Button
-            variant="text"
-            ripple={true}
-            fullWidth={false}
-            className="shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
-            onClick={clearCart}
-          >
-            <BsFillTrashFill size={15} />
-          </Button>
-        </Tooltip>
-      </div>
+      {cart.length > 0 && (
+        <div className="flex flex-row justify-between text-center">
+          <Typography variant="h3" className="text-pinkLogo text-center my-3">
+            Productos
+          </Typography>
+
+          <Tooltip content="Eliminar todos los productos del carrito">
+            <Button
+              variant="text"
+              ripple={true}
+              fullWidth={false}
+              className="shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+              onClick={clearCart}
+            >
+              <BsFillTrashFill size={15} />
+            </Button>
+          </Tooltip>
+        </div>
+      )}
       {cart.map((product) => {
         return (
           <div
@@ -70,32 +74,55 @@ const Cart = ({ cart, clearCart, deleteProductById, total }) => {
           </div>
         );
       })}
-      <div className="border-t border-blue-gray-50 py-1 flex justify-between mt-3 ">
-        <div className="my-3">
-          <Typography variant="h5" className="text-left">
-            Subtotal
-          </Typography>
-          <Typography
-            color="gray"
-            className="text-left text-blue-gray-500 flex items-center"
-          >
-            El envío se calculará al finalizar la compra.
-          </Typography>
+      {cart.length !== 0 ? (
+        <div>
+          <div className="border-t border-blue-gray-50 py-1 flex justify-between mt-3 ">
+            <div className="my-3">
+              <Typography variant="h5" className="text-left">
+                Subtotal
+              </Typography>
+              <Typography
+                color="gray"
+                className="text-left text-blue-gray-500 flex items-center"
+              >
+                El envío se calculará al finalizar la compra.
+              </Typography>
+            </div>
+            <Typography variant="h5" className="text-center my-3">
+              ${total}
+            </Typography>
+          </div>
+          <Link to="/checkout">
+            <Button
+              ripple={true}
+              fullWidth={true}
+              className=" flex items-center justify-center"
+            >
+              Finalizar Compra
+            </Button>
+          </Link>
+          <div className="flex items-center justify-center mt-5 gap-3"></div>
         </div>
-        <Typography variant="h5" className="text-center my-3">
-          ${total}
-        </Typography>
-      </div>
-      <Link to="/checkout">
-        <Button
-          ripple={true}
-          fullWidth={true}
-          className=" flex items-center justify-center"
-        >
-          Finalizar Compra
-        </Button>
-      </Link>
-      <div className="flex items-center justify-center mt-5 gap-3"></div>
+      ) : (
+        <div className="flex flex-col items-center justify-center m-10 p-3 bg-gray-50">
+          <BiSolidShoppingBags size={100} className="mb-10 text-gray-600" />
+          <Typography variant="h4" className="text-center mt-1">
+            ¡Empieza un carrito de compras!
+          </Typography>
+          <Typography variant="lead" className="text-center mt-4 mb-1">
+            Sumá productos y distruta.
+          </Typography>
+          <Link to="/">
+            <Button
+              ripple={true}
+              fullWidth={true}
+              className=" flex items-center justify-center mt-10 bg-pinkLogo"
+            >
+              Descubrir productos
+            </Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
